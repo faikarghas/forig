ef2255fa32ffa30942a403c5e34a955eee52b8ea
faikar <ghassanfaikar@yahoo.com>
2019-09-04 01:55:47 +0700

    commit

--- pages/cardstory.js
+++ pages/cardstory.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Container,Row,Col } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
 
 import '../sass/cardstory.scss'
 
@@ -8,20 +9,120 @@ export class cardstory extends Component {
     render() {
         return (
             <React.Fragment>
-                <header>
+                <header style={{position:'relative'}}>
                     <Container>
-                        <Row>
+                        <Row className="text-center">
+                            <div className="center-abs">
+                                <Fade bottom>
+                                    <div className="text-center">
+                                        <h1 style={{fontWeight:'bold',color:'#8d793e'}}>PASSION</h1>
+                                        <h1 style={{fontWeight:'bold',color:'#8d793e'}}>FOR QUALITY</h1>
+                                    </div>
+                                </Fade>
+                            </div>
                         </Row>
                     </Container>
                 </header>
                 <section className="satu">
                     <Container>
                         <Row>
-                            <Col className="text-center width100">
-                                <h2>Playing Cards Like Never Before</h2>
-                                <p>Here at Card Story, we greatly value Playing card as ana Art.</p>
-                                <p>Here at Card Story, we greatly value Playing card as ana Art.</p>
-                                <img src="../static/image/cardbanyak.jpg" width="100%"/>
+                            <Col className="text-center pad">
+                                <Fade bottom>
+                                    <h2 className="mb-5">Playing Cards Like Never Before</h2>
+                                </Fade>
+                                <Fade>
+                                    <p>Here at Card Story, we greatly value Playing card as ana Art.</p>
+                                    <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                                </Fade>
+                            </Col>
+                        </Row>
+                    </Container>
+                </section>
+                <section className="dua">
+                    <Container>
+                        <Row>
+                            <Col className="text-center pad center-abs">
+                                <h2 className="mb-5">NEW TO PLAYING CARDS?</h2>
+                                <p>Let's see what a playing cards can do!</p>
+                            </Col>
+                        </Row>
+                    </Container>
+                </section>
+                <section className="tiga">
+                    <Container>
+                        <Row>
+                            <Col md={3}>
+                                <div class="cardku">
+                                    <div class="cardku__side cardku__side--front cardku__side--front-1">
+                                        <div class="cardku__description">
+                                            <img src="../static/image/fathom_600px.png" width="100%"/>
+                                        </div>
+                                    </div>
+                                    <div class="cardku__side cardku__side--back cardku__side--back-1">
+                                        <div class="cardku__description">
+                                            <img src="../static/image/fathom-back_600px.png" width="100%"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p className="fs-bold">Fathom</p>
+                                <p>IDR 100,000</p>
+                            </Col>
+                            <Col md={3}>
+                                <div class="cardku">
+                                    <div class="cardku__side cardku__side--front cardku__side--front-1">
+                                        <div class="cardku__description">
+                                            <img src="../static/image/little-island_600px.png" width="100%"/>
+                                        </div>
+                                    </div>
+                                    <div class="cardku__side cardku__side--back cardku__side--back-1">
+                                        <div class="cardku__description">
+                                            <img src="../static/image/little-island-back_600px.png" width="100%"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p className="fs-bold">Island</p>
+                                <p>IDR 100,000</p>
+                            </Col>
+                            <Col md={3}>
+                                <div class="cardku">
+                                    <div class="cardku__side cardku__side--front cardku__side--front-1">
+                                        <div class="cardku__description">
+                                            <img src="../static/image/ltd_600px.png" width="100%"/>
+                                        </div>
+                                    </div>
+                                    <div class="cardku__side cardku__side--back cardku__side--back-1">
+                                        <div class="cardku__description">
+                                            <img src="../static/image/Faikar.jpg" width="100%"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p className="fs-bold">LTD.</p>
+                                <p>IDR 100,000</p>
+                            </Col>
+                            <Col md={3}>
+                                <div class="cardku">
+                                    <div class="cardku__side cardku__side--front cardku__side--front-1">
+                                        <div class="cardku__description">
+                                            <img src="../static/image/pressers_600px.png" width="100%"/>
+                                        </div>
+                                    </div>
+                                    <div class="cardku__side cardku__side--back cardku__side--back-1">
+                                        <div class="cardku__description">
+                                            <img src="../static/image/pressers-back_600px.png" width="100%"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <p className="fs-bold">Pressers</p>
+                                <p>IDR 100,000</p>
+                            </Col>
+                        </Row>
+                    </Container>
+                </section>
+                <section className="empat">
+                    <Container>
+                        <Row>
+                            <Col className="text-center center-abs">
+                                <h2 className="">Free Shipping Order Over IDR 500,000</h2>
                             </Col>
                         </Row>
                     </Container>
